22e2cc683756bd83ea8a08fc1c893405bbba40c7
ats2008 <aravindsugunan@gmail.com>
2024-01-03 02:42:13 +0530

    Updates the template script to be explicitly use the commands from /opt/cms/entrypoint.sh   - everything form the entrypoint.sh except the piece that need sudo is kept   - at a later-point this code block can be swapped with `source /opt/cms/entrypoint.sh`

--- templates/runScript.tpl.sh
+++ templates/runScript.tpl.sh
@@ -5,31 +5,33 @@ if [[ -z "${_CONDOR_SCRATCH_DIR}" ]]; then
 else
     echo CONDOR_SCRATCH_DIR is $_CONDOR_SCRATCH_DIR
 fi
-
-set -x
-export JOBDIR=@@DIRNAME
-export DESTINATION=@@DESTINATION
-echo $PWD
 cd $_CONDOR_SCRATCH_DIR
 mkdir workdir
 chmod -R 777 workdir
 cat >container_runScript.sh <<EOL
+## -- ## from /opt/cms/entrypoint.sh 
+set  -e
 cd /code
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-scram p CMSSW_7_6_7
-cd CMSSW_7_6_7/src/
-git clone -b 2015MiniAOD https://github.com/ats2008/PhysObjectExtractorTool.git
+echo "Setting up \${CMSSW_VERSION}"
+source \${CMS_INSTALL_DIR}/cmsset_default.sh
+scramv1 project CMSSW \${CMSSW_VERSION}
+cd \${CMSSW_VERSION}/src
+eval \`scramv1 runtime -sh\`
+echo "CMSSW should now be available."
+echo "This is a standalone image for ${CMSSW_VERSION} ${SCRAM_ARCH}."
+export LD_LIBRARY_PATH=\${UPDATE_PATH}/lib:\${UPDATE_PATH}/lib64:\${LD_LIBRARY_PATH}
+export PATH=\${UPDATE_PATH}/bin:\${PATH}
+export GIT_EXEC_PATH=\${UPDATE_PATH}/libexec/git-core
+## -- ##
+
+git clone -b 2015MiniAOD https://github.com/cms-opendata-analyses/PhysObjectExtractorTool.git
 scram b -j 4
-cmsenv
 cd $_CONDOR_SCRATCH_DIR
 cmsRun /code/CMSSW_7_6_7/src/PhysObjectExtractorTool/PhysObjectExtractor/python/poet_cfg.py  @@ISDATA inputFiles=@@FNAMES maxEvents=@@MAXEVENTS outputFile=outfile_@@IDX.root tag=@@TAG
-pwd
-ls
-cp *.root $DESTINATION  
-exit 1
+cp *.root @@DESTINATION  
 EOL
 cat container_runScript.sh
 chmod +x container_runScript.sh
 apptainer exec --writable-tmpfs --bind $_CONDOR_SCRATCH_DIR --bind workdir/:/code --bind @@DESTINATION docker://cmsopendata/cmssw_7_6_7-slc6_amd64_gcc493 ./container_runScript.sh
-echo is Sucess : $?
+echo exit code from the execution in container : $?
 rm container_runScript.sh
